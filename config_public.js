1624bea2d11c1020495af7f42d8e7cd9ffa36688
arizi8333 <56826577+arizi8333@users.noreply.github.com>
2019-11-19 21:14:31 +0700

    Update config_public.js

--- config_public.js
+++ config_public.js
@@ -2,6 +2,6 @@
 var ipServerHotel="http://gissurya.org/hotel_bkt/";
 var ipServerTourism="http://gissurya.org/tourism_bkt/";*/
 
-var ipServerAngkot = "http://localhost/iisi/angkot_bkt/";
+var ipServerAngkot = "http://13.76.160.55/angkot_bkt/angkot_bkt/";
 var ipServerHotel = "http://localhost/html/hotel_bkt/";
 var ipServerTourism = "http://localhost/html/tourism_bkt/";
